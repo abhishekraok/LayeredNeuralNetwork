9c4d9be35939bae6c9af171d18c9acd7f6da53fd
abhishekraok <abhishekrao5002@gmail.com>
2017-02-26 22:44:30 -0800

    Refactor into binary school

--- layeredneuralnetwork/school.py
+++ layeredneuralnetwork/school.py
@@ -11,54 +11,56 @@ import layered_neural_network
 import numpy as np
 
 
-def teach_and(classifier):
-    """
-    Teaches the AND task
+class Binary:
+    @staticmethod
+    def teach_and(classifier):
+        """
+        Teaches the AND task
 
-    :type classifier: layered_neural_network.LayeredNeuralNetwork
-    :return: F1 Score in learning this task
-    """
-    return teach_binary_function(classifier, 'and', lambda a, b: a and b)
+        :type classifier: layered_neural_network.LayeredNeuralNetwork
+        :return: F1 Score in learning this task
+        """
+        return Binary.teach_binary_function(classifier, 'and', lambda a, b: a and b)
 
+    @staticmethod
+    def teach_or(classifier):
+        """
+        Teaches the OR task
 
-def teach_or(classifier):
-    """
-    Teaches the OR task
+        :type classifier: layered_neural_network.LayeredNeuralNetwork
+        :return: F1 Score in learning this task
+        """
+        return Binary.teach_binary_function(classifier, 'or', lambda a, b: a or b)
 
-    :type classifier: layered_neural_network.LayeredNeuralNetwork
-    :return: F1 Score in learning this task
-    """
-    return teach_binary_function(classifier, 'or', lambda a, b: a or b)
+    @staticmethod
+    def teach_xor(classifier):
+        """
+        Teaches the XOR task.
 
+        Pre-requisite AND task, OR task.
 
-def teach_xor(classifier):
-    """
-    Teaches the XOR task.
+        :type classifier: layered_neural_network.LayeredNeuralNetwork
+        :return: F1 Score in learning this task
+        """
+        return Binary.teach_binary_function(classifier, 'xor', lambda a, b: int(a) ^ int(b))
 
-    Pre-requisite AND task, OR task.
+    @staticmethod
+    def teach_binary_function(classifier, label, fx):
+        """
+        Teaches the classifier to calculate a binary function fx
 
-    :type classifier: layered_neural_network.LayeredNeuralNetwork
-    :return: F1 Score in learning this task
-    """
-    return teach_binary_function(classifier, 'xor', lambda a, b: int(a) ^ int(b))
-
-
-def teach_binary_function(classifier, label, fx):
-    """
-    Teaches the classifier to calculate a binary function fx
-
-    :type classifier: layered_neural_network.LayeredNeuralNetwork
-    :type label: str
-    :param fx: a function that takes two input and returns a single output
-    :return: F1 Score in learning this task
-    """
-    print('Teaching classifier task of ' + label)
-    classifier_dimension = classifier.input_dimension
-    if classifier_dimension < 2:
-        raise ValueError('Need classifier with at least input dimension of 2')
-    sample_size = 100
-    X = np.random.randn(sample_size, classifier_dimension)
-    X[:, :2] = np.random.randint(low=0, high=2, size=[sample_size, 2], dtype=np.int)
-    Y = np.array([fx(i, j) for i, j in zip(X[:, 0], X[:, 1])], dtype=np.int)
-    classifier.fit(X, Y, label=label)
-    return classifier.score(X, Y, label)
+        :type classifier: layered_neural_network.LayeredNeuralNetwork
+        :type label: str
+        :param fx: a function that takes two input and returns a single output
+        :return: F1 Score in learning this task
+        """
+        print('Teaching classifier task of ' + label)
+        classifier_dimension = classifier.input_dimension
+        if classifier_dimension < 2:
+            raise ValueError('Need classifier with at least input dimension of 2')
+        sample_size = 100
+        X = np.random.randn(sample_size, classifier_dimension)
+        X[:, :2] = np.random.randint(low=0, high=2, size=[sample_size, 2], dtype=np.int)
+        Y = np.array([fx(i, j) for i, j in zip(X[:, 0], X[:, 1])], dtype=np.int)
+        classifier.fit(X, Y, label=label)
+        return classifier.score(X, Y, label)
--- layeredneuralnetwork/test_school.py
+++ layeredneuralnetwork/test_school.py
@@ -7,27 +7,27 @@ class TestLayeredNeuralNetworkSchool(unittest.TestCase):
     def test_train_and_score(self):
         input_dimension = 9
         model = LayeredNeuralNetwork(input_dimension=input_dimension)
-        score = school.teach_and(model)
+        score = school.Binary.teach_and(model)
         self.assertEqual(1, score)
 
     def test_train_or_score(self):
         input_dimension = 9
         model = LayeredNeuralNetwork(input_dimension=input_dimension)
-        score = school.teach_or(model)
+        score = school.Binary.teach_or(model)
         self.assertEqual(1, score)
 
     def test_train_xor_score_fail_without_pre_requisite(self):
         input_dimension = 9
         model = LayeredNeuralNetwork(input_dimension=input_dimension)
-        score = school.teach_xor(model)
+        score = school.Binary.teach_xor(model)
         self.assertLess(score, 0.7)
 
     def test_train_xor_score_succeed_with_pre_requisite(self):
         input_dimension = 9
         model = LayeredNeuralNetwork(input_dimension=input_dimension)
-        and_score = school.teach_and(model)
+        and_score = school.Binary.teach_and(model)
         self.assertEqual(1, and_score)
-        or_score = school.teach_or(model)
+        or_score = school.Binary.teach_or(model)
         self.assertEqual(1, or_score)
-        xor_score = school.teach_xor(model)
+        xor_score = school.Binary.teach_xor(model)
         self.assertEqual(1, xor_score)
