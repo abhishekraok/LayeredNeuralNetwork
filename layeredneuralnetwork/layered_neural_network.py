57dbe6d669600e1fd9c065ce7b7f5c338cde8da4
abhishekraok <abhishekrao5002@gmail.com>
2017-02-26 18:44:57 -0800

    Added train function

--- layeredneuralnetwork/layered_neural_network.py
+++ layeredneuralnetwork/layered_neural_network.py
@@ -1,5 +1,10 @@
 import numpy as np
 import node_manager
+import node
+from sklearn import svm
+import transform_function
+
+retrain_threshold_f1_score = 0.9
 
 
 class LayeredNeuralNetwork():
@@ -7,6 +12,7 @@ class LayeredNeuralNetwork():
         self.input_dimension = input_dimension
         self.node_manager = node_manager.NodeManager(input_dimension)
         self.label_to_node_name = {}
+        self.labels = []
 
     def train(self, X, Y, label):
         """
@@ -16,11 +22,39 @@ class LayeredNeuralNetwork():
         :param X: numpy matrix of size (samples, features)
         :param Y: numpy array of integers with values 0,1
         :type label: str
-        :rtype: None
+        :rtype: bool
+        :return: whether retrained
         """
-        pass
+        print('Training for label ' + label)
+        if label in self.labels:
+            score = self.score(X, Y, label)
+            if score > retrain_threshold_f1_score
+                print('Label {0} already exists with score {1}. Not retraining'.format(label, score))
+                return False
+            else:
+                print('Label {0} exists with score {1}. Retraining'.format(label, score))
 
-    def predict(self, X , label):
+        sample_count = X.shape[0]
+        input_and_features = np.zeros(shape=[sample_count, self.input_dimension + len(self.labels)])
+        input_and_features[:, :self.input_dimension] = X
+        input_and_features[:, self.input_dimension:] = self.activate_all(X)
+        linear_svc = svm.LinearSVC(penalty='l1')
+        linear_svc.fit(input_and_features, Y)
+        score = linear_svc.score(X, Y)
+        print('Trained new Linear SVC with score ' + str(score))
+        learned_transform_function = transform_function.LinearTransformFunction(
+            input_dimension=input_and_features.shape[1],
+            weights=linear_svc.coef_,
+            bias=linear_svc.intercept_)
+
+        node_name = label + '_' + str(np.random.randint(low=0, high=999))
+        new_node = node.Node(name=node_name, input_names=self.node_manager.get_input_names() + self.labels,
+                             transform_function=learned_transform_function,
+                             node_manager=self.node_manager,
+                             is_input=False)
+        self.node_manager.add_node(new_node)
+
+    def predict(self, X, label):
         """
         Predicts whether given X belongs to class "label".
 
@@ -35,6 +69,18 @@ class LayeredNeuralNetwork():
         features = self.node_manager.get_output(X, node_name)
         return np.array(features > 0, dtype=np.int)
 
+    def activate_all(self, X):
+        """
+        Activates all the labels.
+
+        :rtype: np.array
+        :return: numpy array with dimension (samples, learned label count)
+        """
+        sample_count = X.shape[0]
+        result = np.zeros(shape=[sample_count, self.feature_count()])
+        for i, label in enumerate(self.labels):
+            result[:, i] = self.node_manager.get_output(X, label)
+        return result
 
     def identify(self, X):
         """
@@ -43,12 +89,12 @@ class LayeredNeuralNetwork():
         :return: guessed class name
         :rtype: str
         """
-        max_label = self.label_to_node_name.keys()[0]
-        max_strength = 0
-        for label in self.label_to_node_name.keys():
-            features = self.node_manager.get_output(X, label)
-            feature_strength = (features**2).mean()
-            if feature_strength > max_strength:
-                max_strength = feature_strength
-                max_label = label
-        return max_label
+        features = self.activate_all(X)
+        max_index = np.argmax((features ** 2).mean(axis=0))
+        return self.labels[max_index]
+
+    def feature_count(self):
+        return len(self.labels)
+
+    def score(self, X, Y, label):
+        pass
