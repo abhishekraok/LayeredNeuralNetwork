52f72ca8d4dce80b8a6238e4c9cc3f03f3267f62
abhishekraok <abhishekrao5002@gmail.com>
2017-02-27 21:32:17 -0800

    Add travis and setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='layered-neural-network',
+    version='0.0.1',
+    description='Transfer Learning Neural Network',
+    author='Abhishek Rao',
+    author_email='abhishek.rao.comm@gmail.com',
+    url='https://github.com/abhishekraok/LayeredNeuralNetwork',
+)
+
